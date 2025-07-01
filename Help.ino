950c8dce6b0e26f7ad2862e528ced20df99d3256
Aditya Talluri <adi123.talluri@gmail.com>
2025-07-01 16:07:25 +0530

    Guide.ino

---
+++ Help.ino
@@ -0,0 +1,15 @@
+//Hello there, and welcome to my code! My name is Aditya, and I have made a smart dustbin's code in C++ and have used an Arduino Uno Board. This is a very simple code,
+  in which you will find everything you need to operate a smart dustbin, and you can change and edit your code (Not Here!)
+  In this code, you will find:
+
+//1. Which Arduino pin is to be connected to the trigger pin of ultrasonic sensor
+//2. Which Arduino pin is connected to the echo pin of the ultrasonic sensor
+//3. What is the Arduino pin connected to the signal pin of servo motor
+//4. What is the Maximum distance threshold for triggering servo (in centimeters).
+
+//IMPORTANT: You will not be able to see any preview (Sorry for the inconvenience) because you will need to use ARDUINO IDE and upload it to your servo motor and arduino uno.
+ I will post where you can find it in another document. But if you would like to use a guide, you can go to www.roboarmy.in and you can also get a kit.
+
+Link to buy Arduino Uno : https://www.amazon.in/PAGALCODER-Original-Atmega328p-Projects-Prototyping/dp/B0BRQRMN2W/ref=sr_1_1_sspa?dib=eyJ2IjoiMSJ9.IbnmiNVN7ot86MxGBPaPFIUlfbX3gdrxapHeIithFt5Tyb70jwYlEEbro5UBI0AIyT4qjJuWJCRt5rsg6kgW9dfDPlmFFOqMIzltdRtyTFbxb4L5wGkVCpBL8TAPx7iMPseaLCsGOSrnCWe_bnqL6qP-S31lcHp8FmCBPNS9jz1v9UzFydaTpYE_C1h1yGyPEzhV8KPQfKZJQGg5LDhyjNBGOZN2otjBypljSdpg55q5qJaNP7tpFra6fVzfHprZ3_kkfvrGS2N05mVJ6GWTJEIVyYEsJTV8RsycGI9eaWk.TYsYhG0VPPSlyFLrkRDOjEd4CdvryA_hnSZBYZgrQZ8&dib_tag=se&keywords=arduino%2Buno&nsdOptOutParam=true&qid=1751366111&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1
+
+Link to kit: https://www.amazon.in/Young-Innovator-STEM-Robotics-10-15/dp/B0F1CM4GMX/ref=sr_1_5?crid=I28WK4IG3GUH&dib=eyJ2IjoiMSJ9.GpGeGK7MjXLRA5rjF1AR3LH2oj_jBrPxK09U8M4pNwcxPG85HUmdCv5RvzJcnpoCC5NYaUeO62Zyt_0GXmTbGvKfjCfqtkqHlBCQMcAlodnGfgEYlqC1v4ThstaVwLMfr5dnfw0u55fJl9AQuF_Fot0cWDK2xnT5d8L_XZJLMTyI3NivXyIaONnrRB9dLlSndgghlGiQSe9nc3bjeUTcg6_O2IVSKv7UNEPNS8q57QLGSO_WnbWv95SZmRX3zM-AwK8UtEa67oV1sPUICI6yZ5_fWLF6VsPxbST0T5kghnQ.G9_wUjTLA6Q1NOgNGcnxIIpdec1aG3WnkR1wVh0SeI8&dib_tag=se&keywords=roboarmy+smart+dustbin&qid=1751366162&sprefix=roboarmy+smart+dustbin%2Caps%2C211&sr=8-5
